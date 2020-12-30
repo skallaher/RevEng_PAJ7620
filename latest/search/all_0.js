c2fe856deb22604090fa6e0cf3d97ae38721e35b
skallaher <skallaher@users.noreply.github.com>
2020-12-30 00:11:21 +0000

    deploy: 350237e9805db11659859ce79054190a52d591a4

--- latest/search/all_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['bank0_0',['BANK0',['../RevEng__PAJ7620_8h.html#a80dfe389bb69b0ef3a4a0c044615b583ad496eb0cfd42f085f339c7846cd53cd0',1,'RevEng_PAJ7620.h']]],
-  ['bank1_1',['BANK1',['../RevEng__PAJ7620_8h.html#a80dfe389bb69b0ef3a4a0c044615b583a7787d8168e4dacf407e54b5f22b8e256',1,'RevEng_PAJ7620.h']]],
-  ['bank_5fe_2',['Bank_e',['../RevEng__PAJ7620_8h.html#a80dfe389bb69b0ef3a4a0c044615b583',1,'RevEng_PAJ7620.h']]],
-  ['begin_3',['begin',['../classRevEng__PAJ7620.html#ad892a719a660597053ae6532f4b636ed',1,'RevEng_PAJ7620::begin()'],['../classRevEng__PAJ7620.html#a8ae666a13e09c633fa04be4a52844b0b',1,'RevEng_PAJ7620::begin(TwoWire *chosenWireHandle)']]]
-];
--- latest/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['examples_2edoc_95',['examples.doc',['../examples_8doc.html',1,'']]]
-];
